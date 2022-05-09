12198ec6c67b72d7c081cd1b1dc7c0915cb3d04f
Aaditya Menon <aaditya@sparker0i.me>
2022-05-09 14:45:04 +0530

    Pascal Triangle Code

--- src/main/java/me/sparker0i/arrays/PascalTriangle.java
+++ src/main/java/me/sparker0i/arrays/PascalTriangle.java
@@ -16,6 +16,14 @@ public class PascalTriangle {
      * @return Generated Pascal Triangle
      */
     public int[][] runner(int n) {
-        return null;
+        int[][] triangle = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i)
+                    triangle[i][j] = 1;
+                else triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+        return triangle;
     }
 }
